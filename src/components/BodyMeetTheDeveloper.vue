1a2b05ed55adfa480c99cc01539bfbf6cef69b04
ericstretton <eric.stretton@gmail.com>
2022-04-13 19:17:18 -0600

    Hopefully got the git to work.

---
+++ src/components/BodyMeetTheDeveloper.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Eric Stretton</h1>
+        <p>Ball is life yo?!</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PageBody.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>Let's see what cool stuff we can put in here</p>
+        <BodyMeetTheDeveloper/>
+    </div>
+</template>
+
+<script>
+import BodyMeetTheDeveloper from './BodyMeetTheDeveloper.vue'
+import BodyMeetTheDeveloperVue from './BodyMeetTheDeveloper.vue'
+    export default {
+  components: { BodyMeetTheDeveloper },
+        BodyMeetTheDeveloperVue,
+    }
+</script>
+
+<style scoped>
+*{
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+}
+p{
+    grid-row-start: 0;
+    grid-column-start: 0;
+    grid-column-end: 3;
+}
+
+</style>
